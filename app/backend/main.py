2973e0cdb41698f0e6b7f834028ea2c98c635bc1
Lyndon Lim <hnghng127@gmail.com>
2024-07-31 23:52:54 +0800

    Added /voice endpoint and updated README for backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,3 +1,8 @@
 from app import create_app
+from dotenv import load_dotenv
 
 app = create_app()
+
+if __name__ == "__main__":
+    load_dotenv("../.azure/hhgai-dev-eastus-001/.env")
+    app.run(host='0.0.0.0', port=8000, debug=True)
