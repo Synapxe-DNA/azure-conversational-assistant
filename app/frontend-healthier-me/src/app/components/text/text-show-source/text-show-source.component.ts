b657a07cb28c3dbc2074f0949c5047c541d15db6
Gowri Bhat <gowri.bhat@u.nus.edu>
2024-08-01 16:39:06 +0800

    Add other components

---
+++ app/frontend-healthier-me/src/app/components/text/text-show-source/text-show-source.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-show-source',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-show-source.component.html',
+  styleUrl: './text-show-source.component.css'
+})
+export class TextShowSourceComponent {
+
+}
---
+++ app/frontend-healthier-me/src/app/components/text/text-tts/text-tts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-tts',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-tts.component.html',
+  styleUrl: './text-tts.component.css'
+})
+export class TextTtsComponent {
+
+}
