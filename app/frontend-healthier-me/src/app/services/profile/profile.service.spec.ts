28c59d14cc888cbdfe3ab164860add65450dfc91
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-01 18:28:17 +0800

    Frontend -- Fixed test cases

--- app/frontend-healthier-me/src/app/services/profile/profile.service.spec.ts
+++ app/frontend-healthier-me/src/app/services/profile/profile.service.spec.ts
@@ -3,14 +3,23 @@ import { TestBed } from "@angular/core/testing";
 import { ProfileService } from "./profile.service";
 import { NgxIndexedDBModule, NgxIndexedDBService } from "ngx-indexed-db";
 import { NgxIndexedDbConfig } from "../../configs/ngx-indexed-db/ngx-indexed-db.config";
+import { ActivatedRoute } from "@angular/router";
 
 describe("ProfileService", () => {
   let service: ProfileService;
 
   beforeEach(() => {
+    const activatedRouteMock = {
+      snapshot: { paramMap: { get: () => "1234" } },
+    };
+
     TestBed.configureTestingModule({
       imports: [NgxIndexedDBModule.forRoot(NgxIndexedDbConfig)],
-      providers: [ProfileService, NgxIndexedDBService],
+      providers: [
+        ProfileService,
+        NgxIndexedDBService,
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+      ],
     });
     service = TestBed.inject(ProfileService);
   });
