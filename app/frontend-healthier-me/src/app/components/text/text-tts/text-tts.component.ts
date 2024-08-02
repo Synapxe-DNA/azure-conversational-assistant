8cdb38a751a313685f9ba636d95a691b42bd8212
Gowri Bhat <gowri.bhat@u.nus.edu>
2024-08-02 14:20:25 +0800

    Fix tts naming

--- app/frontend-healthier-me/src/app/components/text/text-tts/text-tts.component.ts
+++ app/frontend-healthier-me/src/app/components/text/text-tts/text-tts.component.ts
@@ -1,21 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { Button } from 'primeng/button';
-
+import { Component, Input } from "@angular/core";
+import { Button } from "primeng/button";
 
 @Component({
-  selector: 'app-text-tts',
+  selector: "app-text-tts",
   standalone: true,
-  imports: [Button
-  ],
-  templateUrl: './text-tts.component.html',
-  styleUrl: './text-tts.component.css'
+  imports: [Button],
+  templateUrl: "./text-tts.component.html",
+  styleUrl: "./text-tts.component.css",
 })
 export class TextTtsComponent {
   @Input() message!: string;
 
-  speechToText() {
+  textToSpeech() {
     // Implement speech to text functionality here
-    console.log("Speech to Text");
+    console.log("text to speech");
   }
-
 }
