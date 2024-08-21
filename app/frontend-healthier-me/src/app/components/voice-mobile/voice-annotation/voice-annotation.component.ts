4f42a9dd3b3a77747d827617e29617ee10ef3293
Varstak <e0725939@u.nus.edu>
2024-08-21 11:02:15 +0800

    Updated Source Component

--- app/frontend-healthier-me/src/app/components/voice-mobile/voice-annotation/voice-annotation.component.ts
+++ app/frontend-healthier-me/src/app/components/voice-mobile/voice-annotation/voice-annotation.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { Button } from 'primeng/button';
+import { Component } from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-voice-annotation',
+  selector: "app-voice-annotation",
   standalone: true,
-  imports: [
-    Button
-  ],
-  templateUrl: './voice-annotation.component.html',
-  styleUrl: './voice-annotation.component.css'
+  imports: [ButtonModule, DialogModule, FormsModule],
+  templateUrl: "./voice-annotation.component.html",
+  styleUrls: ["./voice-annotation.component.css"],
 })
 export class VoiceAnnotationComponent {
+  displayModal: boolean = false;
+  feedback: string = "";
 
+  submitFeedback(rating: number) {
+    console.log("Rating selected:", rating);
+    this.displayModal = true;
+  }
 }
--- app/frontend-healthier-me/src/app/components/voice-mobile/voice-sources/voice-sources.component.ts
+++ app/frontend-healthier-me/src/app/components/voice-mobile/voice-sources/voice-sources.component.ts
@@ -1,47 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
-import { AccordionModule } from 'primeng/accordion';
-import { MessageSource } from '../../../types/message.type';
+import { Component, Input, OnInit } from "@angular/core";
+import { CarouselModule } from "primeng/carousel";
+import { AccordionModule } from "primeng/accordion";
+import { MessageSource } from "../../../types/message.type";
 
-const sources = [
-  {
-    "title": "TITLE",
-    "description": "DESCRIPTION",
-    "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
-    "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
-  },
-  {
-    "title": "TITLE",
-    "description": "DESCRIPTION",
-    "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
-    "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
-  },
-  {
-    "title": "TITLE",
-    "description": "DESCRIPTION",
-    "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
-    "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
-  },
-  {
-    "title": "TITLE",
-    "description": "DESCRIPTION",
-    "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
-    "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
-  },
-]
+// const sources = [
+//   {
+//     "title": "TITLE",
+//     "description": "DESCRIPTION",
+//     "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
+//     "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
+//   },
+//   {
+//     "title": "TITLE",
+//     "description": "DESCRIPTION",
+//     "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
+//     "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
+//   },
+//   {
+//     "title": "TITLE",
+//     "description": "DESCRIPTION",
+//     "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
+//     "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
+//   },
+//   {
+//     "title": "TITLE",
+//     "description": "DESCRIPTION",
+//     "cover_image_url": "https://ch-api.healthhub.sg/api/public/content/5e1a87e404c44a26ae253d77976e630a?v=26491741&t=w347",
+//     "url": "https://www.healthhub.sg/live-healthy/smoking-alcohol-and-drugs---why-teens-get-hooked-on-this-triple-threat"
+//   },
+// ]
+
+const sources: MessageSource[] = [];
 
 @Component({
-  selector: 'app-voice-sources',
+  selector: "app-voice-sources",
   standalone: true,
-  imports: [
-    CarouselModule,
-    AccordionModule,
-  ],
-  templateUrl: './voice-sources.component.html',
-  styleUrl: './voice-sources.component.css'
+  imports: [CarouselModule, AccordionModule],
+  templateUrl: "./voice-sources.component.html",
+  styleUrl: "./voice-sources.component.css",
 })
-export class VoiceSourcesComponent implements OnInit{
-
+export class VoiceSourcesComponent implements OnInit {
   responsiveOptions: any[] | undefined;
 
   @Input() sources: MessageSource[] = sources;
@@ -49,10 +47,10 @@ export class VoiceSourcesComponent implements OnInit{
   ngOnInit(): void {
     this.responsiveOptions = [
       {
-          breakpoint: '768px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
-}
+        breakpoint: "768px",
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
 }
