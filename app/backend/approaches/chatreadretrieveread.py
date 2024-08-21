d5aff2aeb18a4233ca65fe931887306b8692a850
jwyting <139433726+jwyting@users.noreply.github.com>
2024-08-21 18:08:57 +0800

    lint

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -22,7 +22,9 @@ from openai.types.chat import (
     ChatCompletionToolParam,
 )
 from openai_messages_token_helper import build_messages, get_token_limit
+
 # from lingua import Language, LanguageDetectorBuilder
+
 
 class ChatReadRetrieveReadApproach(ChatApproach):
     """
@@ -121,7 +123,9 @@ class ChatReadRetrieveReadApproach(ChatApproach):
         minimum_reranker_score = config.MINIMUM_RERANKER_SCORE
         response_token_limit = config.CHAT_RESPONSE_MAX_TOKENS
 
-        selected_language = config.SELECTED_LANGUAGE #to be removed. variable to come from frontend user selection instead of config file
+        selected_language = (
+            config.SELECTED_LANGUAGE
+        )  # to be removed. variable to come from frontend user selection instead of config file
 
         if profile.user_age < 1:
             age_group = "Infant"
@@ -177,9 +181,7 @@ class ChatReadRetrieveReadApproach(ChatApproach):
 
         if profile.profile_type == "general":
             query_prompt = general_query_prompt
-            answer_generation_prompt = general_prompt.format(
-                selected_language=selected_language
-            )
+            answer_generation_prompt = general_prompt.format(selected_language=selected_language)
         else:
             query_prompt = profile_query_prompt.format(
                 gender=profile.user_gender,
