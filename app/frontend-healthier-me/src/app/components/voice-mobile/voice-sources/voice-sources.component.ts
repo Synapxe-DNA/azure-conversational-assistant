2b659da1cc2985069dee93901f19a7935843aacd
Varstak <e0725939@u.nus.edu>
2024-08-25 21:01:16 +0800

    Updated Sources Component

--- app/frontend-healthier-me/src/app/components/voice-mobile/voice-sources/voice-sources.component.ts
+++ app/frontend-healthier-me/src/app/components/voice-mobile/voice-sources/voice-sources.component.ts
@@ -1,25 +1,25 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { CarouselModule } from "primeng/carousel";
 import { AccordionModule } from "primeng/accordion";
 import { MessageSource } from "../../../types/message.type";
-
 
 @Component({
   selector: "app-voice-sources",
   standalone: true,
   imports: [CarouselModule, AccordionModule],
   templateUrl: "./voice-sources.component.html",
-  styleUrl: "./voice-sources.component.css",
+  styleUrls: ["./voice-sources.component.css"],
 })
 export class VoiceSourcesComponent implements OnInit {
   responsiveOptions: any[] | undefined;
+  hoveredImageUrl: string | null = null; // Variable to hold the hovered image URL
 
   @Input() sources: MessageSource[] = [];
+  imgUrl: string = "assets/healthhub-logo.png"; // Updated path
 
   ngOnInit(): void {
-
     console.log("voice-sources input", this.sources);
-    
+
     this.responsiveOptions = [
       {
         breakpoint: "768px",
@@ -27,5 +27,27 @@ export class VoiceSourcesComponent implements OnInit {
         numScroll: 1,
       },
     ];
+  }
+
+  // Method to get the image URL
+  getImageUrl(coverImageUrl: string | null): string {
+    // Check if coverImageUrl is 'None' or falsy and return the fallback image URL
+    console.log("coverImageUrl: ", coverImageUrl);
+    return coverImageUrl && coverImageUrl !== "None"
+      ? coverImageUrl
+      : this.imgUrl;
+  }
+
+  // Method to log the image URL
+  logImageUrl(url: string | null): void {
+    if (url && url !== "None") {
+      console.log("Image URL:", url);
+      this.hoveredImageUrl = url; // Set the hovered image URL
+    }
+  }
+
+  // Method to clear the log
+  clearLog(): void {
+    this.hoveredImageUrl = null; // Clear the hovered image URL
   }
 }
