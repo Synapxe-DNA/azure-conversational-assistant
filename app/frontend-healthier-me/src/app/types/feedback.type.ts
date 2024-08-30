bcb99f05b9417066874c2637794e87c9c5679273
Lyndon Lim <hnghng127@gmail.com>
2024-08-31 01:07:02 +0800

    Update feedback(frontend/backend) and fix pulsing

--- app/frontend-healthier-me/src/app/types/feedback.type.ts
+++ app/frontend-healthier-me/src/app/types/feedback.type.ts
@@ -3,7 +3,7 @@ import { Profile } from "./profile.type";
 
 export interface Feedback {
   label: string;
-  category: string;
+  category: string[];
   remarks: string;
   chat_history: Message[];
   profile_id: string;
