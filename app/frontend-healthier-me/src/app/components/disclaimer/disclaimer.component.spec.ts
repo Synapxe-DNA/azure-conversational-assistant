151be3f4496b8f531b45100e6a81a0dbbf366f7c
Gowri Bhat <gowri.bhat@u.nus.edu>
2024-09-20 09:42:48 +0800

    Update linting

--- app/frontend-healthier-me/src/app/components/disclaimer/disclaimer.component.spec.ts
+++ app/frontend-healthier-me/src/app/components/disclaimer/disclaimer.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { DisclaimerComponent } from './disclaimer.component';
+import { DisclaimerComponent } from "./disclaimer.component";
 
-describe('DisclaimerComponent', () => {
+describe("DisclaimerComponent", () => {
   let component: DisclaimerComponent;
   let fixture: ComponentFixture<DisclaimerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DisclaimerComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DisclaimerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
